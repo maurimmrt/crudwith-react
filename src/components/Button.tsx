fc119532ce0cb48234e360ea7036216d0ab9406b
goddmartee <goddmartee@gmail.com>
2024-03-06 18:30:06 -0600

    Errors Solved: Data types of the parameters missing for bulding production

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,17 @@
-export function Button({ className, onClick, children }) {
-	return (
-		<button
-			type='button'
-			className={`p-2 ms-3 ${className}`}
-			onClick={onClick}
-		>
-			{children}
-		</button>
-	);
+import { FC, MouseEventHandler, ReactNode } from 'react';
+
+interface Button {
+	className: string;
+	onClick: MouseEventHandler<HTMLButtonElement>;
+	children: ReactNode;
 }
+
+export const Button: FC<Button> = ({ className, onClick, children }) => (
+	<button
+		type='button'
+		className={`p-2 ms-3 ${className}`}
+		onClick={onClick}
+	>
+		{children}
+	</button>
+);
